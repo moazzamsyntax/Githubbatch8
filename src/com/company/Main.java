77c1c129b8a73d62b9dc30639879533a6718482d
moazzamsyntax <moazzamsyntax@gmail.com>
2021-01-23 14:42:50 -0500

    kkkl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,10 +12,7 @@ public class Main {
         Mercedes.seatingCapacity="2";
         Mercedes.name="Mecar";
         
-        HI these are some new features that i your friend is adding on the project 
-        please have a look at them and test them once 
-          i shall be very thnakful to you
-
+       
 
 
 
