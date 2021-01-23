2296706659f421f260e5d7414fba9929df67b10d
moazzamsyntax <moazzamsyntax@gmail.com>
2021-01-23 10:24:14 -0500

    making a car

---
+++ src/com/company/Car.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Car {
+
+    String name;
+    String number;
+    String model;
+    String engineCapacity;
+    String seatingCapacity;
+    String color;
+
+}
