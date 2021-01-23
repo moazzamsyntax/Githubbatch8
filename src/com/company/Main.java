2b632adbdbff56c64c1d1120dadcc125e377d340
moazzamsyntax <moazzamsyntax@gmail.com>
2021-01-23 13:52:54 -0500

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         Car Mercedes= new Car();
+        Mercedes.color="Black";
+        Mercedes.number="4000";
+        Mercedes.model="QAX787";
+        Mercedes.engineCapacity="1500cc";
+        Mercedes.seatingCapacity="2";
+        Mercedes.name="Mycar";
 
 
 
