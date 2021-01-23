0c49a655d8a282d2e67110c235c318ae4e7b3a7e
moazzamsyntax <moazzamsyntax@gmail.com>
2021-01-23 13:39:01 -0500

    kk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Car Mercedes= new Car();
-        Mercedes.color="Black";
-        Mercedes.number="4000";
-        Mercedes.model="QAX787";
-        Mercedes.engineCapacity="1500cc";
-        Mercedes.seatingCapacity="2";
-        Mercedes.name="Mycar";
+        
 
 
     }
